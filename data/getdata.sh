d469af53c2727e3fc74b2aebb6715531a11ceb3c
JP Bida <jpbida@ambrosevaast.com>
2015-09-07 11:38:01 -0700

    Data Source

---
+++ data/getdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+### Gets dataset for analysis ###
+
+# http://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Part-D-Prescriber.html
+
+# Data dictionary
+# https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Downloads/Prescriber_Methods.pdf 
+
+wget http://download.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Downloads/PartD_Prescriber_PUF_NPI_DRUG_13.zip
